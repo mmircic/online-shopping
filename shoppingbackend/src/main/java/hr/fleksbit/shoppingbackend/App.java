8b7701b20ba3e7ece551c640c3634f5147dd7b08
Mateo Mirčić <mateo1406@gmail.com>
2018-10-18 09:01:05 +0200

    My Project Setup Completted Succesfully

---
+++ shoppingbackend/src/main/java/hr/fleksbit/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package hr.fleksbit.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
